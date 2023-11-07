0fa84473881e5a8763487508e393a2caf1291b25
gorkem.tutuncu <gorkem.tutuncu@monsternotebook.com>
2023-11-07 15:33:53 +0300

    interfaces were defined in core layer

---
+++ Survey.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync(); //igenericrepository ile farklılık olsun diye örnek olması açısından yaptık burayı
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);  //Iservicede bu değişiklikleri yansıtacağımız için void değil, artık burada TASK tipinde olmalı update remove ve removerange
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+
+    }
+}
